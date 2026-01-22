421311b2658a56c4fc3c812c9d04d274ddfc79b5
iZedikus <izedikus555@mail.ru>
2026-01-22 16:12:23 +0700

    init commit

---
+++ src/main/java/ru/stepanov/publication_service/PublicationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.stepanov.publication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stepanov/publication_service/PublicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanov.publication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
