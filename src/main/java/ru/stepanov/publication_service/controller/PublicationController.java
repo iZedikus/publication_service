f03264a124f1fedebf4d8fed07fa549333ca4959
iZedikus <izedikus555@mail.ru>
2026-01-24 16:38:25 +0700

    PublicationDTOs

--- src/main/java/ru/stepanov/publication_service/controller/PublicationController.java
+++ src/main/java/ru/stepanov/publication_service/controller/PublicationController.java
@@ -1,7 +1,5 @@
 package ru.stepanov.publication_service.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,90 +11,73 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.stepanov.publication_service.model.Publication;
-import ru.stepanov.publication_service.repository.PublicationRepository;
+import ru.stepanov.publication_service.entity.Publication;
+import ru.stepanov.publication_service.model.request.PublicationCreateRequest;
+import ru.stepanov.publication_service.model.response.PublicationResponse;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor
-@Slf4j
 public class PublicationController {
     @PostMapping("/publications")
     @Transactional
-    public ResponseEntity<Publication> createPublication(@RequestBody Publication publication) {
-        log.debug("Post request to save publication: {}", publication);
-
-        //TODO repo call
-
-        log.info("Publication with ID {} was created", );
-        log.debug("Publication data: ");
+    public PublicationResponse createPublication(@RequestBody PublicationCreateRequest publicationCreateRequest) {
+        //TODO service call
     }
 
     @GetMapping("/publications/{id}")
     @Transactional
-    public ResponseEntity<Publication> getPublicationById(@PathVariable UUID id) {
-        log.debug("Request to get publication with ID: {}", id);
+    public PublicationResponse getPublicationById(@PathVariable UUID id) {
         //TODO repo call
     }
 
     @GetMapping("/publications/")
     @Transactional
-    public List<Publication> getAllPublicationsByAuthor(@RequestParam UUID authorId) {
-        log.debug("Request to get publication with authorID: {}", authorId);
+    public List<PublicationResponse> getAllPublicationsByAuthor(@RequestParam UUID authorId) {
         //TODO
     }
 
     @GetMapping("/publications/")
     @Transactional
-    public List<Publication> getAllPublicationsByCategory(@RequestParam UUID categoryId) {
-        log.debug("Request to get publication with categoryID: {}", categoryId);
+    public List<PublicationResponse> getAllPublicationsByCategory(@RequestParam UUID categoryId) {
         //TODO
     }
 
     @GetMapping("/publications/")
     @Transactional
-    public List<Publication> getAllPublicationsByCitation(@RequestParam UUID citationId) {
-        log.debug("Request to get publication with citationID: {}", citationId);
+    public List<PublicationResponse> getAllPublicationsByCitation(@RequestParam UUID citationId) {
         //TODO
     }
 
     @GetMapping("/publications/")
     @Transactional
-    public List<Publication> getAllPublicationsByEdition(@RequestParam UUID editionId) {
-        log.debug("Request to get publication with editionID: {}", editionId);
+    public List<PublicationResponse> getAllPublicationsByEdition(@RequestParam UUID editionId) {
         //TODO
     }
 
     @GetMapping("/publications/")
     @Transactional
-    public List<Publication> getAllPublicationsByIssue(@RequestParam UUID issueId) {
-        log.debug("Request to get publication with issueID: {}", issueId);
+    public List<PublicationResponse> getAllPublicationsByIssue(@RequestParam UUID issueId) {
         //TODO
     }
 
     @GetMapping("/publications/")
     @Transactional
-    public List<Publication> getAllPublicationsByKeyword(@RequestParam UUID keywordId) {
-        log.debug("Request to get publication with keywordID: {}", keywordId);
+    public List<PublicationResponse> getAllPublicationsByKeyword(@RequestParam UUID keywordId) {
         //TODO
     }
 
     @PutMapping("/publications/")
     @Transactional
-    public ResponseEntity<Publication> updatePublication(@RequestBody Publication publication) {
-        log.debug("Put request to update publication: {}", publication);
+    public ResponseEntity<PublicationResponse> updatePublication(@RequestBody Publication publication) {
         //TODO
-        log.info("Publication with ID {} was updated", );
-        log.debug("Publication data: {}", );
     }
 
     @DeleteMapping("/publications/{id}")
     @Transactional
     public ResponseEntity<Publication> deletePublication(@PathVariable UUID id) {
-        log.debug("Request to delete publication with ID: {}", id);
         //TODO
     }
 }
